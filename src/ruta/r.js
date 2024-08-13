623578d3afea5568662dc0a4382ab020886828bc
leonel <gun45780@gmail.com>
2024-08-13 17:29:33 -0500

    subir a render

---
+++ src/ruta/r.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { controlarMaquina, estadoMaquina } from '../controlador/c.js';
+
+const router = Router();
+
+// Rutas para controlar la máquina y obtener el estado
+router.post('/control', controlarMaquina);
+router.get('/status', estadoMaquina);
+
+export default router;
