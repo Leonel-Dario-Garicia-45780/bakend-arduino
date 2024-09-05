2cc3bb0c4d02bb27ffba233862964dbd5b444c78
leonel <gun45780@gmail.com>
2024-09-05 17:33:18 -0500

    1.1  comunicacion con la placa (get)

--- src/controlador/c.js
+++ src/controlador/c.js
@@ -1,150 +1,73 @@
+//! intento 2 de comunicacion de placa y fronent
 
-//! comunicacion placa y fronent
-
-
-
-
-import https from 'https';
-
+// Variables globales
 let horaApagado = null;
 let maquinaEncendida = false;
-// const esp32IpAddress = '192.168.1.100'; // Reemplaza con la IP de tu placa ESP32
-const esp32IpAddress = '192.168.1.104/control'; // Reemplaza con la IP de tu placa ESP32
-const esp32Port = 80; // Reemplaza con el puerto de tu placa ESP32
+let ultimaSolicitudESP32 = null;
 
-// Controla la máquina: encender o apagar
+// Función para controlar la máquina
 export const controlarMaquina = async (req, res) => {
   const { command, hour, minute } = req.body;
 
+  // Encender la máquina
   if (command === 'on') {
     maquinaEncendida = true;
     console.log('Máquina encendida');
-    try {
-      const options = {
-        method: 'POST',
-        hostname: esp32IpAddress,
-        port: esp32Port,
-        path: '/control',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      };
-
-      const req = https.request(options, (res) => {
-        let data = '';
-        res.on('data', (chunk) => {
-          data += chunk;
-        });
-        res.on('end', () => {
-          console.log(`Respuesta de la placa ESP32: ${data}`);
-          res.status(200).json({ status: 'success', command });
-        });
-      });
-
-      req.write(JSON.stringify({ command: 'on' }));
-      req.end();
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ status: 'error', message: 'Error al enviar la orden a la placa ESP32' });
-    }
-    return; // Salir después de encender la máquina
+    res.status(200).json({ status: 'success', command });
+    return;
   }
 
+  // Apagar la máquina
   if (command === 'off') {
+    // Apagado programado
     if (hour !== undefined && minute !== undefined) {
-      // Programar apagado
       horaApagado = { hour: parseInt(hour), minute: parseInt(minute) };
       console.log(`Hora de apagado programada a las ${hour}:${minute}`);
-      try {
-        const options = {
-          method: 'POST',
-          hostname: esp32IpAddress,
-          port: esp32Port,
-          path: '/control',
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        };
-
-        const req = https.request(options, (res) => {
-          let data = '';
-          res.on('data', (chunk) => {
-            data += chunk;
-          });
-          res.on('end', () => {
-            console.log(`Respuesta de la placa ESP32: ${data}`);
-            res.status(200).json({ status: 'success', command, message: `Hora de apagado programada a las ${hour}:${minute}` });
-          });
-        });
-
-        req.write(JSON.stringify({ command: 'off_at', hour, minute }));
-        req.end();
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ status: 'error', message: 'Error al enviar la orden a la placa ESP32' });
-      }
+      res.status(200).json({ status: 'success', command, message: `Hora de apagado programada a las ${hour}:${minute}` });
     } else {
-      // Apagar inmediatamente
+      // Apagado inmediato
       maquinaEncendida = false;
       console.log('Máquina apagada');
-      try {
-        const options = {
-          method: 'POST',
-          hostname: esp32IpAddress,
-          port: esp32Port,
-          path: '/control',
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        };
-
-        const req = https.request(options, (res) => {
-          let data = '';
-          res.on('data', (chunk) => {
-            data += chunk;
-          });
-          res.on('end', () => {
-            console.log(`Respuesta de la placa ESP32: ${data}`);
-            res.status(200).json({ status: 'success', command, message: 'Máquina apagada' });
-          });
-        });
-
-        req.write(JSON.stringify({ command: 'off' }));
-        req.end();
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ status: 'error', message: 'Error al enviar la orden a la placa ESP32' });
-      }
+      res.status(200).json({ status: 'success', command, message: 'Máquina apagada' });
     }
-    return; // Salir después de procesar el apagado
+    return;
   }
 
   res.status(400).json({ status: 'error', message: 'Comando no reconocido' });
 };
 
-// Devuelve el estado actual de la máquina
+// Ruta para verificar el estado de la máquina
 export const estadoMaquina = (req, res) => {
+  ultimaSolicitudESP32 = new Date();  // Registrar la última vez que la ESP32 hizo una solicitud
   res.status(200).json({ maquinaEncendida, horaApagado });
+};
+
+// Ruta para informar al frontend sobre la última solicitud de la ESP32
+export const ultimaSolicitud = (req, res) => {
+  const ahora = new Date();
+  let diferencia = (ahora - ultimaSolicitudESP32) / 1000; // Diferencia en segundos
+
+  // Si han pasado más de 10 segundos desde la última solicitud, no se considera reciente
+  const esReciente = diferencia <= 10;
+  
+  res.status(200).json({
+    ultimaSolicitudESP32,
+    esReciente
+  });
 };
 
 
 
 
 
+// //! comunicacion placa y fronent (no funciono)
 
-
-
-
-
-
-////////////////////////////////
-// // import cron from 'node-cron';
 // import https from 'https';
 
 // let horaApagado = null;
 // let maquinaEncendida = false;
 // // const esp32IpAddress = '192.168.1.100'; // Reemplaza con la IP de tu placa ESP32
-// const esp32IpAddress = '192.168.1.104'; // Reemplaza con la IP de tu placa ESP32
+// const esp32IpAddress = '192.168.1.104/control'; // Reemplaza con la IP de tu placa ESP32
 // const esp32Port = 80; // Reemplaza con el puerto de tu placa ESP32
 
 // // Controla la máquina: encender o apagar
@@ -261,87 +184,6 @@ export const estadoMaquina = (req, res) => {
 // export const estadoMaquina = (req, res) => {
 //   res.status(200).json({ maquinaEncendida, horaApagado });
 // };
-
-
-
-
-
-//! un pqueño problemita con axios en render
-/*
-// import cron from 'node-cron';
-// import axios from 'axios';
-
-// let horaApagado = null;
-// let maquinaEncendida = false;
-// const esp32IpAddress = '192.168.1.100'; // Reemplaza con la IP de tu placa ESP32
-// const esp32Port = 80; // Reemplaza con el puerto de tu placa ESP32
-
-// // Controla la máquina: encender o apagar
-// export const controlarMaquina = async (req, res) => {
-//   const { command, hour, minute } = req.body;
-
-//   if (command === 'on') {
-//     maquinaEncendida = true;
-//     console.log('Máquina encendida');
-//     try {
-//       const response = await axios.post(`http://${esp32IpAddress}:${esp32Port}/control`, { command: 'on' });
-//       res.status(200).json({ status: 'success', command });
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ status: 'error', message: 'Error al enviar la orden a la placa ESP32' });
-//     }
-//     return; // Salir después de encender la máquina
-//   }
-
-//   if (command === 'off') {
-//     if (hour !== undefined && minute !== undefined) {
-//       // Programar apagado
-//       horaApagado = { hour: parseInt(hour), minute: parseInt(minute) };
-//       console.log(`Hora de apagado programada a las ${hour}:${minute}`);
-//       try {
-//         const response = await axios.post(`http://${esp32IpAddress}:${esp32Port}/control`, { command: 'off_at', hour, minute });
-//         res.status(200).json({ status: 'success', command, message: `Hora de apagado programada a las ${hour}:${minute}` });
-//       } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ status: 'error', message: 'Error al enviar la orden a la placa ESP32' });
-//       }
-//     } else {
-//       // Apagar inmediatamente
-//       maquinaEncendida = false;
-//       console.log('Máquina apagada');
-//       try {
-//         const response = await axios.post(`http://${esp32IpAddress}:${esp32Port}/control`, { command: 'off' });
-//         res.status(200).json({ status: 'success', command, message: 'Máquina apagada' });
-//       } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ status: 'error', message: 'Error al enviar la orden a la placa ESP32' });
-//       }
-//     }
-//     return; // Salir después de procesar el apagado
-//   }
-
-//   res.status(400).json({ status: 'error', message: 'Comando no reconocido' });
-// };
-
-// // Devuelve el estado actual de la máquina
-// export const estadoMaquina = (req, res) => {
-//   res.status(200).json({ maquinaEncendida, horaApagado });
-// };
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
